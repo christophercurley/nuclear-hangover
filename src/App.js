b3ebc22e352a9d95e789a922bcdfd40b6d8fceb9
Christopher Curley <clevercurley@gmail.com>
2022-09-13 18:31:02 -0400

    adds hero

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
+      <Hero />
       <h1>nuclear hangover</h1>
     </div>
   );
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Hero.css";
+import hero from "../assets/hero.jpg";
+
+function Hero() {
+  return (
+    <div>
+      <img src={hero} />
+    </div>
+  );
+}
+
+export default Hero;
