c452f90e1507e9ea9205c48fbe3ff05f4dc9b8f2
Christopher Curley <clevercurley@gmail.com>
2022-09-13 23:37:12 -0400

    adds media queries to Hero, changes title and favicon

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Hero.css";
 import hero from "../assets/hero.jpg";
 import logo from "../assets/logo.png";
 
 function Hero() {
+  // const [sWidth, setSWidth] = useState(null)
+
+  // useEffect(() => {
+  //   let
+  // }, [])
+
   return (
-    <div>
+    <>
       <img className="hero" src={hero} />
       <img className="logo" src={logo} />
-    </div>
+    </>
   );
 }
 
