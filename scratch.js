04743d23b9db641a2adce0631c1fc5ee94d98389
Christopher Curley <clevercurley@gmail.com>
2022-09-18 00:58:32 -0400

    removed scratch.js

--- scratch.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-const url = "https://ghibliapi.herokuapp.com/films";
-
-const getData = () => {
-  return axios
-    .get(url)
-    .then((response) => response.data[0].title)
-    .catch((error) => console.log("WE HAVE AN ERROR :)"));
-};
